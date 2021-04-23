3e7652ff89579177b0c283759ed0c9a31615cf4a
Rishitosh Sawant <srishis.214@gmail.com>
2021-04-23 15:39:53 -0700

    Update and rename poly.sv to casting_and_polmorphism.sv

--- casting_and_polmorphism.sv
+++ casting_and_polmorphism.sv
@@ -1,14 +1,28 @@
+/*
+Notes on Casting:
+Upcasting is parent to child type assignment and its legal
+Downcasting is child to parent type assignment and it is NOT legal in OOP
+Hence, we need to explicitly perform a run-time (dynamic) typecasting check to ensure
+that both the object handles on LHS and RHS are of same type using $cast(dest, src) system task in SV
+Same type in classes means they belong to the same class hierarchy
+Ex: class Animal has child classes like dog, cat, bird
+but it cannot have a unrelated child class like car or food as these are not related to animals
+But how it needs appears in real Verification test bench?
+Usually our TB env is complex with handles to classes of various types
+and to make sure that the class handles on LHS and RHS are compatible we need to use $cast()
+This ensures that we avoid a compilation error as $cast() froces the check to happen at run-time
+*/
 module test;
   	class parent;
-  	virtual function void print();
-    		$display("parent class");
-  	endfunction : print
+  		virtual function void print();
+    			$display("parent class");
+  		endfunction : print
 	endclass : parent
 
 	class child extends parent;
- 	 virtual function void print();
-   	 	$display("child class");
- 	 endfunction : print
+ 		 virtual function void print();
+   	 		$display("child class");
+ 	 	endfunction : print
 	endclass : child
     // create handles (pointers) of both classes
     parent p, p1; 
@@ -26,7 +40,7 @@ module test;
     $display("CASE II: Polymorphism");
     c = new();
     p = c;		// shallow copy assign child to parent
-    p.print(); 	// if print method is declared virtual, "child class" is printed else "parent class" is printed.
+    p.print(); 		// if print method is declared virtual, "child class" is printed else "parent class" is printed.
     c.print();
     $display("******************************");
     end
