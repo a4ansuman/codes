a2b5a02bbeca2af2cbbba95f4ef97e262fc4d4cf
Rishitosh Sawant <srishis.214@gmail.com>
2021-04-27 02:40:43 -0700

    Create copy_queue.sv

---
+++ copy_queue.sv
@@ -0,0 +1,4 @@
+// copy queue contents to dynamic array
+//1) I want to copy queue 1 into dynamic array and print it
+//2) I want to copy dynmaic array into another queue 2
+//3) Then copy queue 2 partially to queue 3 and print it
